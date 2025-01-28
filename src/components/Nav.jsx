2752d310b9f9e2f837ded58d4f801edf3b90a634
amith p <amithanilp@gmail.com>
2025-01-28 20:32:32 +0530

    nav bar is completed

---
+++ src/components/Nav.jsx
@@ -0,0 +1,31 @@
+import "./Nav.css";
+const Nav = () => {
+  return (
+    <>
+      <div className="container">
+        <div className="logo">
+          <h2>
+            <a href="#">react app</a>
+          </h2>
+        </div>
+        <div className="menu">
+          <ul>
+            <li>
+              <a href="#">home</a>
+            </li>
+            <li>
+              <a href="#">about</a>
+            </li>
+            <li>
+              <a href="#">services</a>
+            </li>
+            <li>
+              <a href="#">contact</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </>
+  );
+};
+export default Nav;
