75799925b412e0209a9fc4b51e828f0ddd4cef53
amith p <amithanilp@gmail.com>
2025-01-30 12:23:23 +0530

    Card issue

---
+++ src/components/Cards/Card.jsx
@@ -0,0 +1,18 @@
+import "./Card.css";
+
+const Card = () => {
+  return (
+    <>
+      <div className="card-container">
+        <Cards /><Cards /> <Cards /> <Cards />
+      </div>
+    </>
+  );
+};
+export default Card;
+
+const Cards = () => {
+  return <div className="cards">
+    
+  </div>;
+};
--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,21 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import "./main.css"
 import Nav from "./components/Nav/Nav.jsx"
 import Aside from "./components/Aside/Aside.jsx"
+import Card from"./components/Cards/Card.jsx"
+
 
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
+    <div className="container-main">
     <Nav />
+    <div className="main-cnt">
     <Aside />
+    <Card />
+    <Card /><Card /><Card /><Card /><Card /><Card /><Card />
+    </div>
+    </div>
   </StrictMode>,
 )
